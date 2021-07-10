7c638094f76266c3f523307af6841f884d24c262
nitin reddy <reddynitin3@gmail.com>
2021-07-10 22:44:27 +0530

    Added a project to work with single spa

---
+++ angular-app/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { APP_BASE_HREF } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [{ provide: APP_BASE_HREF, useValue: '/angular' }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
